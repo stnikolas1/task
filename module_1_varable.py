dfdb46b76a9c9edf64051a56f81631654dccc925
stnikolas1 <osipchuk6868@gmail.com>
2024-10-09 19:21:23 +0300

    Add files via upload

---
+++ module_1_varable.py
@@ -0,0 +1,5 @@
+Number_of_homework_completed = 12
+Number_of_hours_spent = 1.5
+Course_title = 'Python'
+Time_for_one_task = Number_of_hours_spent / Number_of_homework_completed
+print('Курс:',Course_title+', всего задач:',str(Number_of_homework_completed)+', затрачено часов:',str(Number_of_hours_spent)+', среднее время выполнения',Time_for_one_task,'часа')
