67ae7a718b1e06ed48ce4d5d1d08e6f970c95b16
stnikolas1 <osipchuk6868@gmail.com>
2024-10-23 01:26:46 +0300

    Update ancient_cipher.py

--- ancient_cipher.py
+++ ancient_cipher.py
@@ -1,18 +1,26 @@
 print('Введите число от 3 до 20')
 n = int(input())
-list_ = []
+result = []
+a = 0
 if 3 <= n <= 20 and n % 2 == 0:
     for i in range(1, int((n / 2) + 1)):
-            for j in range(n, 1, -1):
-                if n % (i + j) == 0 and i != j:
-                    list_.append(i)
-                    list_.append(j)
-
+            for j in range(2, n):
+                if n % (i + j) == 0 and i != j and (result.count(j *10 + i) != True or j == 11):
+                    if j - 10 < 0:
+                        a = j + i * 10
+                        result.append(a)
+                    else:
+                        a = j + i * 100
+                        result.append(a)
 else:
     for i in range(1, int(((n-1) / 2) + 1)):
-            for j in range(n, 1, -1):
+            for j in range(2, n):
                 if n % (i + j) == 0 and i != j:
-                    list_.append(i)
-                    list_.append(j)
+                    if j - 10 < 0:
+                        a = i * 10 + j
+                        result.append(a)
+                    else:
+                                a = i * 100 + j
+                                result.append(a)
 
-print('Для числа',n,'-',list_)
+print('Для числа',n,'-',result)
